fcb109343f41a26898acb55a8be8207b3ad4793f
shivam pandey <sp5812070@gmail.com>
2026-04-28 10:06:25 +0530

    java Loops

---
+++ Proimitive.java
@@ -0,0 +1,26 @@
+package Java;
+
+public class Proimitive {
+    public static void main(String args[]) {
+        /*
+         * Primitive Data Types in Java
+         * Java has 8 primitive data types
+         */
+        //Byte
+        byte b = 100;
+        //short
+        short s = 10000;
+        //int
+        int x = 100000;
+        //long
+        long l = 10000000000L;
+        //float
+        float f = 3.14f;
+        //double
+        double d = 3.14159;
+        //char
+        char c = 'A';
+        //boolean
+        boolean flag = true;
+    }
+}
